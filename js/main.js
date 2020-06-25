7df744e17769ea353117ab405cb45899476108b6
claire_t <e.claire.thompson@gmail.com>
2020-06-25 16:55:16 -0400

    Mobile styles

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const menuButton = document.getElementById('menu-button');
+const navMenu = document.getElementById('nav');
+const navItems = document.getElementById('nav-items');
+
+const toggleNav = () => {
+    if (navMenu.style.height == '245px') {
+        navItems.style.height = '0px'
+        navMenu.style.height = '0px';
+        menuButton.className = 'fas fa-bars';
+    } else {
+        navMenu.style.height = '245px';
+        navItems.style.height = '200px';
+        menuButton.className = 'fas fa-times';
+    }
+};
+
+menuButton.addEventListener('click', toggleNav)
