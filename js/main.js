7334576b51f8773a4d26d5538603f5dd3b5f0878
claire_t <e.claire.thompson@gmail.com>
2020-06-29 14:16:14 -0400

    Cleaned up main js file

--- js/main.js
+++ js/main.js
@@ -15,20 +15,3 @@ const toggleNav = () => {
 };
 
 menuButton.addEventListener('click', toggleNav);
-
-/* const navSize = () => {
-    if (screen.availWidth < 900) {
-        navMenu.style.height = '0px';
-        navItems.style.height = '0px';
-        menuButton.className = 'fas fa-bars';
-        navMenu.style.transition = 'none';
-        toggleNav();
-    } else if (screen.availWidth >= 900) {
-        navMenu.style.height = 'unset';
-        navItems.style.height = 'unset';
-        navMenu.style.transition = 'none';
-        menuButton.className = 'fas fa-times';
-    }
-}
-
-window.addEventListener('resize', navSize);*/
